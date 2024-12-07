510e3ac0954d4194d5a303babc7ab0ad3041b7e8
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-12-07 19:38:51 +0200

    handel errors that can occured when user don't input city or input wrong city

--- script.js
+++ script.js
@@ -12,19 +12,27 @@ let root = document.documentElement;
 button.addEventListener("click",async()=>{
     let location=input.value;
     let unit=select.value;
-    console.log(location)
-    console.log(unit)
-    let data = await getWeather(todayDate(),location,unit); 
-    console.log(data) 
-    let temperature=jsonProcessing(data).temperature;
-    let humidity=jsonProcessing(data).humidity
-    changeBodyColor(temperature,unit)
-    displayWeather(temperature,humidity)
+    if(!location){
+        alert("You should input location")
+    }else{
+        let data = await getWeather(todayDate(),location,unit); 
+        console.log(data) 
+        let temperature=jsonProcessing(data).temperature;
+        let humidity=jsonProcessing(data).humidity
+        changeBodyColor(temperature,unit)
+        displayWeather(temperature,humidity)
+    }
+    
 })
 async function getWeather(date,city,unit){
+   try{
     let response=await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${date}/${date}?key=3BJSV2AXDNW95EZG9M4G844MK&unitGroup=${unit}&include=current&elements=%2Ctemp%2Chumidity%2Cwinddir`,{mode:"cors"})
     let json=await response.json();
     return json;
+   }
+   catch(e){
+    alert(city+ " is not a city")
+   }
 }
 
 function todayDate(){
